ff390f96e6eaed42b83b76881b14285458cbcd9c
ekbrousseau <49698415+ekbrousseau@users.noreply.github.com>
2019-09-25 17:37:51 -0400

    Update index.php

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>CS 321 Project</title>
+  <!-- author: Emma Brousseau 
+     description: CS 321 Assignment 2 -->
+</head>
+
+<body>
+   <header>
+     <nav>
+    <ul id="navigation">
+      <li><a href="#resources">Resources</a></li>
+      <li><a href="#recommendations">Recommendations</a></li>
+      <li><a href="#quotes">Quotes</a></li>
+      <li><a href="artwork">Artwork and Writing</a></li>
+      <li><a href="about">About</a></li>
+    </ul>
+     </nav>
+  </header>
+  <main>
+    <section>
+  <div id="exist">
+    <h2>Exist as You Are</h2>
+    <p>A website for those affected by eating disorders, whether in recovery, in treatment, hoping to seek treatment, or if you have an affected loved one. This is a place to find resources for help, to find recovery-oriented materials (such as podcasts, blogs, books, etc.), and to see artwork and writing that may be inspirational or motivating, on the good days and the bad.</p>
+<img src = "https://lh3.googleusercontent.com/6vikLLhTgrgFToXIKXsE_l5gFHZZWO7cz6lGEd6rI-dQbD0PbR4DMD_dwbLJzz4YexrNM_1rYZJptvWJrJzSdxM_TuV4_pgNvR5gbPgWatVFCw1vngcVvhY1TwM7Vk5mb0HyRV_82w"alt="Artwork of a person laying on a yellow whale in the night sky, floating over a town. The text on the image is a quote by Walt Whitman that says I exist as I am, that is enough"/>
+  </div>
+    </section>
+    <section>
+  <div id="why">
+    <h2>Behind the Theme</h2>
+    <p>Eating disorders can make it hard to ever feel comfortable in your body. Even in recovery, bad body image days can be really disheartening; having a changing body is difficult, and getting used to a new body takes a lot of time and patience. On the bad days, it can be helpful to remember that existing as you are is enough. Your physical appearance doesn't determine your worth in this world; your existence, exactly as it is right now, is enough.</p>
+  </div>
+      </section>
+  </main>
+  <footer>
+  <p>  By Emma Brousseau</p>
+</footer>
+</body>
+
+</html>
